bd23c79a61c32965920cb6f92426a1a9f3e9b0a8
Viktoria Myronova <ipz212_mvl@student.ztu.edu.ua>
2025-01-20 19:35:31 +0200

    Create component with virtual elements in the More country detsils table

--- src/pages/CountryPage.jsx
+++ src/pages/CountryPage.jsx
@@ -3,7 +3,9 @@ import CountryDetails from "../components/CountryDetails";
 import Quiz from "../components/Quiz";
 import PhrasesList from "../components/PhrasesList";
 import TraditionsList from "../components/TraditionsList";
+import VirtualTable from "../components/VirtualTable";
 import "./CountryPage.css";
+import "./VirtualTable.css";
 
 const CountryPage = ({ country }) => {
   if (!country) {
@@ -23,19 +25,23 @@ const CountryPage = ({ country }) => {
       </section>
 
       <section className="country-content">
-        {country.phrases?.length > 0 ? (
+        {country.phrases?.length > 0 && (
           <div className="phrases-section">
             <PhrasesList phrases={country.phrases} />
           </div>
-        ) : null}
-
-        {country.traditions?.length > 0 ? (
+        )}
+        {country.traditions?.length > 0 && (
           <div className="traditions-section">
             <TraditionsList traditions={country.traditions} />
           </div>
-        ) : null}
+        )}
+        {country.moreDetails && (
+          <div className="details-section">
+            <h2>Country Details</h2>
+            <VirtualTable details={country.moreDetails[0]} />
+          </div>
+        )}
       </section>
-
       <section className="quiz-section">
         <h2>Test Your Knowledge</h2>
         {country.quizQuestions?.length > 0 ? (
